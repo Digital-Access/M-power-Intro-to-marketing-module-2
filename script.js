1dc9232a4a8e7c06c87776dba120f8506b6d634f
Digital-Access <102788087+Digital-Access@users.noreply.github.com>
2022-04-13 16:24:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ const video = document.getElementById('video');
 const progress = document.getElementById('progress');
 const total = document.getElementById('total');
 const progressText = document.getElementById('progressText');
+const navCntrl = document.getElementById('navCntrl');
+const progressDetails = document.getElementById('progressDetails')
 
 progressText.innerText = config.progress_text;
 total.textContent = config.videos.length;
@@ -23,6 +25,8 @@ video.src = config.videos[0];
 
 let i = 0;
 progress.textContent = i + 1;
+prevBtn.style.display = 'none'
+navCntrl.style.justifyContent = 'flex-end'
 
 let max = config.videos.length;
 const nextVideo = () => {
@@ -30,7 +34,14 @@ const nextVideo = () => {
         i++
         video.src = config.videos[i];
     }
+
+    if(i === max -1){
+        nxtBtn.style.display = 'none'
+    }
+
     progress.textContent = i + 1;
+    prevBtn.style.display = 'flex'
+    navCntrl.style.justifyContent = 'space-between'
 }
 
 nxtBtn.addEventListener('click', nextVideo)
@@ -40,9 +51,22 @@ const prevVideo = () => {
     if (i != 0) {
         i--
         video.src = config.videos[i];
+        nxtBtn.style.display = 'flex';
+    } 
+
+    if(i === 0){
+        prevBtn.style.display = 'none'
+        navCntrl.style.justifyContent = 'flex-end'
     }
+
     progress.textContent = i + 1;
 }
 
-
 prevBtn.addEventListener('click', prevVideo)
+
+if (config.videos.length === 1 || config.videos[i] === "") {
+    nxtBtn.style.display = 'none'
+    prevBtn.style.display = 'none'
+    navCntrl.style.justifyContent = 'center'
+    navCntrl.style.padding = '1.25rem 0 1.25rem 0'
+}
